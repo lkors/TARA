bc3b9dbdcb931b5f8dfb0baa87b201c19c4d0c23
Laura Kõrsmaa <laura.korsmaa@gmail.com>
2017-11-30 19:52:52 +0100

    119 Maandumisleht

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+$("#accordion div").click(function(){$(this).find("i").toggleClass("fa-chevron-down fa-chevron-up")});
---
+++ js/script.js
@@ -0,0 +1,3 @@
+$('#accordion div').click(function(){
+    $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up')
+});
