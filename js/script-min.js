cc14bb136b108e33736c924d8c50d04c608d32a9
Laura Kõrsmaa <laura.korsmaa@gmail.com>
2017-11-30 23:26:39 +0100

    119 parandused

--- js/script-min.js
+++ js/script-min.js
@@ -1,1 +1,1 @@
-$("#accordion div").click(function(){$(this).find("i").toggleClass("fa-chevron-down fa-chevron-up")});
+function setFooterStyle(){var o=$(window).height(),e=$("#footer").outerHeight(),t=$("#footer").position().top+e;t<o?$("#footer").css("margin-top",o-t+"px"):$("#footer").css("margin-top",""),$("#footer").removeClass("invisible")}$("#accordion div").click(function(){$(this).find("i").toggleClass("fa-chevron-down fa-chevron-up")}),$(document).ready(function(){setFooterStyle(),window.onresize=setFooterStyle}),$("#accordion").click(function(o){$(".collapse").collapse("hide")});
