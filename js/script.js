c2c54c197984df4619161353bb08b99725f2d478
Laura Kõrsmaa <laura.korsmaa@gmail.com>
2017-12-06 15:08:41 +0100

    Footer in bottom

--- js/script.js
+++ js/script.js
@@ -2,22 +2,6 @@ $('#accordion div').click(function(){
     $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up')
 });
 
-function setFooterStyle() {
-  var docHeight = $(window).height();
-  var footerHeight = $('#footer').outerHeight();
-  var footerTop = $('#footer').position().top + footerHeight;
-  if (footerTop < docHeight) {
-    $('#footer').css('margin-top', (docHeight - footerTop) + 'px');
-  } else {
-    $('#footer').css('margin-top', '');
-  }
-  $('#footer').removeClass('invisible');
-}
-$(document).ready(function() {
-  setFooterStyle();
-  window.onresize = setFooterStyle;
-});
-
 $('#accordion').click(function(e) {
   $('.collapse').collapse('hide');
 });
